89fcf2cfe7cb1576e64da035f7cb5c4838e4878c
Abed-2000 <Abedmia2000@gmail.com>
2023-08-02 03:26:40 -0400

    FIXED: temp Content now gets translated

--- translationTesting.py
+++ translationTesting.py
@@ -3,23 +3,21 @@ from googletrans import Translator
 from translation_split import split_translation_file
 
 scriptPath = os.path.dirname(os.path.abspath(__file__))
-rawPath = os.path.join(scriptPath, "rawMedia")
-tempPath = os.path.join(scriptPath, "temp")
+rawMedia = os.path.join(scriptPath, "rawMedia")
+temp = os.path.join(scriptPath, "temp")
 processedMedia = os.path.join(scriptPath, "processedMedia")
-tranlationNeededFile = os.path.join(rawPath, "shortStory")
-
+tranlationNeededFile = os.path.join(rawMedia, "shortStory")
+languageCode = os.environ["languageCode"]
 translator = Translator()
 
+numTranslationFiles = split_translation_file(tranlationNeededFile, temp)
 
-        
-numTranslationFiles = split_translation_file(tranlationNeededFile, tempPath)
-
-directory = tempPath
+directory = temp
  
 for filename in os.scandir(directory):
     if filename.is_file():
         with open(filename.path, "r") as file:
             translationData = file.read()
-            translation = translator.translate(translationData, dest='bn')
-            finalTranslation = open(os.path.join(rawPath, "translatedText.txt"), "w")
+            translation = translator.translate(translationData, dest=languageCode)
+            finalTranslation = open(os.path.join(rawMedia, "translatedText.txt"), "w")
             finalTranslation.write(translation.text)
