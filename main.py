a94359115da3b2d23e30507f341aea831022245a
Abed-2000 <Abedmia2000@gmail.com>
2023-08-02 03:23:44 -0400

    ADDED: Language code validator

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import os
 from gtts import gTTS
 from PyPDF2 import PdfReader
+import langcodes
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 
-raw_path = os.path.join(script_path, "rawMedia")
+rawMedia = os.path.join(script_path, "rawMedia")
 processedMedia = os.path.join(script_path, "processedMedia")
+temp = os.path.join(script_path, "temp")
 
+def is_valid_language_code(code):
+    try:
+        langcodes.Language.get(code)
+        return True
+    except langcodes.LanguageTagError:
+        return False
+
+def get_valid_language_code():
+    while True:
+        language_code = input("\nEnter the language that you wish for the final translation to be in;\n(ex. for english type \"en\" or for french type \"fr\"): ").strip()
+        if is_valid_language_code(language_code):
+            return language_code
+        else:
+            print("Invalid language code. Please try again.")
 
 file2Convert = input("Enter the name of the file that you would like to convert to an audio format. \n(Please ensure that the file exists within the \"rawMedia\" folder; ex. sample.txt or sample.pdf): ")
+valid_language_code = get_valid_language_code()
+os.environ["languageCode"] = valid_language_code
 
 try:
-    with open(os.path.join(raw_path, file2Convert)) as document:
+    with open(os.path.join(rawMedia, file2Convert)) as document:
         contents = document.read()
-        tts = gTTS(contents, lang = 'bn')
-        tts.save(os.path.join(processedMedia, (file2Convert + "_en.mp3")))
-
+        tts = gTTS(contents, lang = valid_language_code)
+        tts.save(os.path.join(processedMedia, (file2Convert + "_" + valid_language_code + ".mp3")))
 except FileNotFoundError:
     print('File does not exist in the \"rawMedia\" folder.')
